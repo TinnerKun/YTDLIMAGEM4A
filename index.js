f861913ad35c4deb3b70bfdaa432f843c71071fd
ⵝⵉⵏⵏⴻⵔⴿⵓⵏ <45807795+TinnerKun@users.noreply.github.com>
2022-08-03 11:20:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { exec } = require('child_process');
-// const translate = require('translate');
 const fs = require('fs')
 const axios = require('axios');
 const myArgs = process.argv.slice(2);
@@ -35,21 +34,10 @@ async function downloads() {
   axios.get(`https://www.youtube.com/oembed?url=${videoId}&format=json`)
     .then(async function (response) {
       console.log(`Author Name Music [ Don't EN Fix Now!! ] -> ${response.data.author_name}`)
-      // translate.engine = "yandex";
-      // translate.key = '';
-      // const authorset = await translate(response.data.author_name, "en");
-      // console.log(`Author Name Music [ EN Fix Now!! ] -> ${authorset}`)
-      // return;
       let title = `${response.data.title}-${myArgs[0].replace('https://www.youtube.com/watch?v=', '')}`
       let list = /['\/,]/gm
-      //  let list = /['\/,\\_\-\+=|\]\[\*\^]/gm
       title1 = title.replaceAll(list, ' ')
-      //let authorset = response.data.author_name
       exec(`start powershell -Command "${path} --add-metadata --encoding utf8 --embed-thumbnail --output '${title1}.%(ext)s' -f 140 ${videoId}"`, (err, stdout, stderr) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
         console.log(`ytdl -> Download ${videoId} Done`)
         exec(`tageditor.exe -s cover="${title1}.jpg" --max-padding 125000 -f "${title1}.m4a"`, (err, stdout, stderr) => {
           console.log('image set Done');
